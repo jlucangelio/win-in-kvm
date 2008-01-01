e030b9733dacc07364a264413d8b52bc6bea2278
t4m5yn <t4m5yn@d4f9f78e-8842-0410-97ab-7b3d8d7c4250>
2008-01-01 18:54:11 +0000

    Version with forking instead of daemonizing QEMU.

---
+++ tags/fork/qemumonitor.py
@@ -0,0 +1,122 @@
+import time
+import telnetlib
+import logging
+
+class QEMUMonitor:
+	def __init__(self, address, port):
+		self.address	= address
+		self.port	= port
+
+		self.telnet_conn = telnetlib.Telnet(address, port)
+		self.telnet_conn.read_until("(qemu) ")
+		logging.info("Connected to %s:%d", address, port)
+
+	def quit(self):
+		self.telnet_conn.write("quit\n")
+		self.telnet_conn.close()
+
+	def eject_device(self, device, force):
+		if force:
+			cmd = "eject -f %s\n"
+		else:
+			cmd = "eject %s\n"
+
+		self.telnet_conn.write(cmd % device)
+
+	def change_device(self, device, filename):
+		self.telnet_conn.write("change %s %s\n" % device, filename)
+
+	def snapshot(self, action, tag): # load, save, del
+		self.telnet_conn.write("%svm %s\n" % action, tag)
+
+	def stop(self):
+		self.telnet_conn.write("stop\n")
+
+	def cont(self):
+		self.telnet_conn.write("cont\n")
+
+	def sendkey(self, key):
+		self.telnet_conn.write("sendkey %s\n" % key)
+
+	def soft_poweroff(self, password):
+		self.sendkey("ctrl-alt-delete")
+
+		time.sleep(0.1)
+
+		for c in password:
+			key = ""
+
+			if c.isupper():
+				key = "shift-" + c
+			else:
+				key = c
+
+			self.sendkey(key)
+
+		self.sendkey("ret")
+
+		time.sleep(10)
+
+		self.sendkey("ctrl-alt-delete")
+
+		time.sleep(0.1)
+
+		self.sendkey("alt-shift-s")
+
+		time.sleep(0.1)
+
+		for i in range(3):
+			self.sendkey("tab")
+
+		self.sendkey("shift-h")
+
+		for c in "ypervisor":
+			self.sendkey(c)
+
+		self.sendkey("ret")
+
+	def hard_reboot(self): 
+		self.telnet_conn.write("system_reset\n")
+
+	def hard_poweroff(self):
+		self.telnet_conn.write("system_powerdown\n")
+
+# help|? [cmd] -- show the help
+# commit device|all -- commit changes to the disk images (if -snapshot is used) or backing files
+# info subcommand -- show various information about the system state
+# -q|quit  -- quit the emulator
+# -eject [-f] device -- eject a removable media (use -f to force it)
+# -change device filename -- change a removable media
+# screendump filename -- save screen into PPM image 'filename'
+# log item1[,...] -- activate logging of the specified items to '/tmp/qemu.log'
+# -savevm tag|id -- save a VM snapshot. If no tag or id are provided, a new snapshot is created
+# -loadvm tag|id -- restore a VM snapshot from its tag or id
+# -delvm tag|id -- delete a VM snapshot from its tag or id
+# -stop  -- stop emulation
+# -c|cont  -- resume emulation
+# gdbserver [port] -- start gdbserver session (default port=1234)
+# x /fmt addr -- virtual memory dump starting at 'addr'
+# xp /fmt addr -- physical memory dump starting at 'addr'
+# p|print /fmt expr -- print expression value (use $reg for CPU register access)
+# i /fmt addr -- I/O port read
+# -sendkey keys -- send keys to the VM (e.g. 'sendkey ctrl-alt-f1')
+# -system_reset  -- reset the system
+# -system_powerdown  -- send system power down event
+# sum addr size -- compute the checksum of a memory region
+# usb_add device -- add USB device (e.g. 'host:bus.addr' or 'host:vendor_id:product_id')
+# usb_del device -- remove USB device 'bus.addr'
+# cpu index -- set the default CPU
+# mouse_move dx dy [dz] -- send mouse move events
+# mouse_button state -- change mouse button state (1=L, 2=M, 4=R)
+# mouse_set index -- set which mouse device receives events
+# wavcapture path [frequency bits channels] --
+# 	capture audio to a wave file (default frequency=44100 bits=16 channels=2)
+# stopcapture capture index -- stop capture
+# memsave addr size file -- save to disk virtual memory dump starting at 'addr' of size 'size'
+# read_disk_io hdx -- read disk I/O statistics (VMDK format)
+# migrate [-d] command -- migrate the VM using command (use -d to not wait for command to complete)
+# migrate_cancel  -- cancel the current VM migration
+# migrate_set_speed value -- set maximum speed (in bytes) for migrations
+
+#m = QEMUMonitor("localhost", 10000)
+#m.quit()
---
+++ tags/fork/win-in-kvm.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+import sys
+import os
+import time
+import shelve
+import getpass
+
+import qemumonitor
+
+vmdb = shelve.open("vmdbpy")
+
+argc = len(sys.argv)
+
+if argc < 2:
+	print "wik: Usage: %s (add|rm|run) <name> [cmdline] [executable]" % sys.argv[0]
+	sys.exit(1)
+
+cmdlilne = ""
+
+if sys.argv[1] == "add" and argc >= 4:
+	vmdb[sys.argv[2]] = sys.argv[3]
+	vmdb.close()
+	sys.exit(0)
+elif sys.argv[1] == "rm" and argc >= 3:
+	del vmdb[sys.argv[2]]
+	vmdb.close()
+	sys.exit(0)
+elif sys.argv[1] == "run" and argc >= 4:
+	cmdline = vmdb[sys.argv[2]]
+
+
+user = raw_input("wik: Windows username: ")
+passwd = getpass.getpass("wik: Windows password: ")
+
+telnet_port = 10000 + os.getpid()
+
+cmdline = "qemu-system-x86_64 %s -monitor telnet:localhost:%d,server -vnc :1" % (cmdline, telnet_port)
+
+if not os.fork():
+	# child
+	os.system(cmdline)
+else:
+	# parent
+	print "wik: Launched VM"
+	time.sleep(1)
+
+	mon = qemumonitor.QEMUMonitor("localhost", telnet_port)
+	print "wik: Launched monitor"
+	print "wik: Waiting for VM boot"
+
+	for i in range(6):
+		print "wik: %d seconds left" % (60 - i * 10)
+		time.sleep(10)
+
+	os.system("rdesktop -u %s -p %s localhost -s \"%s\" -S standard -D" % (user, passwd, sys.argv[3]))
+
+	print "wik: Shutting down VM"
+	mon.soft_poweroff(passwd)
+
+	os.wait()
